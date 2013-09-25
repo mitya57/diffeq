11ba38c3e9fe4bf9c2db69d04e1476104a6b21cb
Dmitry Shachnev <mitya57@gmail.com>
2013-09-25 22:12:26 +0400

    Really read system from file

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@
 #include "window.hpp"
 
 void fillPolynom(QString line, Polynom &polynom) {
-	 QStringList parts = line.split(" + ");
-	 QStringList subParts;
-	 Monom m;
-	 for (int i = 0; i < parts.size(); ++i) {
-		 subParts = parts.at(i).split(" * ");
-		 m.xPow = subParts.at(1).toDouble();
-		 m.yPow = subParts.at(2).toDouble();
-		 m.c = subParts.at(0).toInt();
-		 polynom += m;
-	 }
+	QStringList parts = line.split("  ");
+	QStringList subParts;
+	Monom m;
+	for (int i = 0; i < parts.size(); ++i) {
+		subParts = parts.at(i).split(' ');
+		m.xPow = subParts.at(1).toDouble();
+		m.yPow = subParts.at(2).toDouble();
+		m.c = subParts.at(0).toInt();
+		polynom += m;
+	}
 }
 
 void fillSystem(QString fileName, PolynomSystem &system) {
@@ -28,9 +28,7 @@ void fillSystem(QString fileName, PolynomSystem &system) {
 int main(int argc, char **argv) {
 	QApplication app(argc, argv);
 	PolynomSystem system;
-	system.first.append({0, 1, 1});
-	system.second.append({1, 0, -1});
-	system.second.append({0, 3, -1});
+	fillSystem("system.txt", system);
 	MyMainWindow window(&system);
 	window.show();
 	return app.exec();
