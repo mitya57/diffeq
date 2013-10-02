f210998797f978b0085bc38c1f03725656b53158
Dmitry Shachnev <mitya57@gmail.com>
2013-10-02 12:36:02 +0400

    Add translations support and Russian translation

---
+++ locale/ru.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>DrawArea</name>
+    <message>
+        <location filename="../window.cpp" line="67"/>
+        <source>Center</source>
+        <translation>Центр</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="68"/>
+        <source>Stable focus</source>
+        <translation>Устойчивый фокус</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="69"/>
+        <source>Unstable focus</source>
+        <translation>Неустойчивый фокус</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="70"/>
+        <source>Stable knot</source>
+        <translation>Устойчивый узел</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="71"/>
+        <source>Unstable knot</source>
+        <translation>Неустойчивый узел</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="72"/>
+        <source>Saddle</source>
+        <translation>Седло</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="100"/>
+        <source>Point type: %1  Scale: %2</source>
+        <translation>Тип точки: %1  Масштаб: %2</translation>
+    </message>
+</context>
+<context>
+    <name>MyMainWindow</name>
+    <message>
+        <location filename="../window.cpp" line="20"/>
+        <source>Draw Mesh</source>
+        <translation>Рисовать сетку</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="26"/>
+        <source>Differential equations</source>
+        <translation>Дифференциальные уравнения</translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+#include <QtCore/QLocale>
+#include <QtCore/QTranslator>
 #include <QtWidgets/QApplication>
 #include "window.hpp"
 
 int main(int argc, char **argv) {
 	QApplication app(argc, argv);
+	QTranslator translator;
+	translator.load(QLocale::system().name(), "locale");
+	app.installTranslator(&translator);
 	PolynomSystem system;
 	MyMainWindow window(&system);
 	window.show();
