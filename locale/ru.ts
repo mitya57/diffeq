dd52eea214ef3d1e1c69408b49514ce58884014f
Dmitry Shachnev <mitya57@gmail.com>
2013-10-10 07:49:07 +0400

    Refresh translations

--- locale/ru.ts
+++ locale/ru.ts
@@ -4,39 +4,54 @@
 <context>
     <name>DrawArea</name>
     <message>
-        <location filename="../window.cpp" line="67"/>
+        <location filename="../window.cpp" line="110"/>
         <source>Center</source>
         <translation>Центр</translation>
     </message>
     <message>
-        <location filename="../window.cpp" line="68"/>
+        <location filename="../window.cpp" line="111"/>
         <source>Stable focus</source>
         <translation>Устойчивый фокус</translation>
     </message>
     <message>
-        <location filename="../window.cpp" line="69"/>
+        <location filename="../window.cpp" line="112"/>
         <source>Unstable focus</source>
         <translation>Неустойчивый фокус</translation>
     </message>
     <message>
-        <location filename="../window.cpp" line="70"/>
+        <location filename="../window.cpp" line="113"/>
         <source>Stable knot</source>
         <translation>Устойчивый узел</translation>
     </message>
     <message>
-        <location filename="../window.cpp" line="71"/>
+        <location filename="../window.cpp" line="114"/>
         <source>Unstable knot</source>
         <translation>Неустойчивый узел</translation>
     </message>
     <message>
-        <location filename="../window.cpp" line="72"/>
+        <location filename="../window.cpp" line="115"/>
+        <source>Stable singular knot</source>
+        <translation>Стабильный вырожденный узел</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="116"/>
+        <source>Unstable singular knot</source>
+        <translation>Нестабильный вырожденный узел</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="117"/>
         <source>Saddle</source>
         <translation>Седло</translation>
     </message>
     <message>
-        <location filename="../window.cpp" line="100"/>
-        <source>Point type: %1  Scale: %2</source>
-        <translation>Тип точки: %1  Масштаб: %2</translation>
+        <location filename="../window.cpp" line="145"/>
+        <source>Parameter: %1;  Point type: %2;  Scale: %3</source>
+        <translation>Параметр: %1;  Тип особой точки: %2;  Масштаб: %3</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="151"/>
+        <source>Period: %1</source>
+        <translation>Период: %1</translation>
     </message>
 </context>
 <context>
@@ -47,7 +62,7 @@
         <translation>Рисовать сетку</translation>
     </message>
     <message>
-        <location filename="../window.cpp" line="26"/>
+        <location filename="../window.cpp" line="27"/>
         <source>Differential equations</source>
         <translation>Дифференциальные уравнения</translation>
     </message>
